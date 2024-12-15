39401f29cf5439595867a04ba378644cf8bc2b06
BugHunter-X <askzenai@gmail.com>
2024-12-15 04:12:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error if the image is not found.
+  const image = new Image();
+  image.src = '/missing-image.jpg';
+
+  return (
+    <div>About Page</div>
+  );
+}
+```
